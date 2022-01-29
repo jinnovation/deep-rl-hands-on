d84f04e8119d349485befb4c48857febeb13474c
Jonathan Jin <me@jonathanj.in>
2022-01-28 19:26:44 -0500

    Finish off Ch 2; integrate Monitor into environment

---
+++ ch2/03-random-action-wrapper.py
@@ -0,0 +1,32 @@
+import random
+from typing import TypeVar
+
+import gym
+
+Action = TypeVar("Action")
+
+
+class RandomActionWrapper(gym.ActionWrapper):
+    epsilon: float
+
+    def __init__(self, env, epsilon: float = 0.1):
+        super().__init__(env)
+        self.epsilon = epsilon
+
+    def action(self, action: Action) -> Action:
+        if random.random() < self.epsilon:
+            print("Random!")
+            return self.env.action_space.sample()
+        return action
+
+
+if __name__ == "__main__":
+    env = RandomActionWrapper(gym.make("CartPole-v0"))
+    obs = env.reset()
+    total_reward = 0.0
+    while True:
+        obs, reward, done, _ = env.step(0)
+        total_reward += reward
+        if done:
+            break
+    print("Reward got: %.2f" % total_reward)
